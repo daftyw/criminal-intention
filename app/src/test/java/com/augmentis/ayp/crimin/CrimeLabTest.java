9800698981226f550ebecedfa42dee082dbfc01b
Rawin Ngamloet <rawin.n@augmentis.biz>
2016-07-25 17:17:43 +0700

    add recycler view

---
+++ app/src/test/java/com/augmentis/ayp/crimin/CrimeLabTest.java
@@ -0,0 +1,32 @@
+package com.augmentis.ayp.crimin;
+
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Rawin on 20-Jul-16.
+ */
+public class CrimeLabTest {
+
+    @Test
+    public void test_create_crimeLab_no_error_and_size_100() {
+        CrimeLab crimeLab = CrimeLab.getInstance();
+        for (int i=0; i<2000000; i++) {
+            Crime crime = crimeLab.getCrimeById(UUID.randomUUID());
+            assertNotNull(crime);
+        }
+
+    }
+
+    @Test
+    public void testArray() {   //
+
+    }
+}
