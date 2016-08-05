f7830437826c6ca1189e9615cab5b1704010e344
Rawin Ngamloet <rawin.n@augmentis.biz>
2016-08-05 10:19:59 +0700

    Combine back the adaptor and view-holder

--- app/src/main/java/com/augmentis/ayp/crimin/CrimeListFragment.java
+++ app/src/main/java/com/augmentis/ayp/crimin/CrimeListFragment.java
@@ -17,13 +17,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.augmentis.ayp.crimin.model.Crime;
+import com.augmentis.ayp.crimin.model.CrimeDateFormat;
 import com.augmentis.ayp.crimin.model.CrimeLab;
+import com.augmentis.ayp.crimin.model.PictureUtils;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Rawin on 18-Jul-16.
@@ -141,7 +147,7 @@ public class CrimeListFragment extends Fragment {
         List<Crime> crimes = crimeLab.getCrimes();
 
         if(_adapter == null) {
-            _adapter = new CrimeListAdapter(this, crimes);
+            _adapter = new CrimeListAdapter(crimes);
             _crimeRecyclerView.setAdapter(_adapter);
         } else {
             _adapter.setCrimes(crimeLab.getCrimes());
@@ -169,5 +175,108 @@ public class CrimeListFragment extends Fragment {
         Log.d(TAG, "Resume list");
         updateUI();
     }
+
+
+    public class CrimeListViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+        private static final String TAG = "CrimeListViewHolder";
+        public TextView _titleTextView;
+        public TextView _dateTextView;
+        public CheckBox _solvedCheckBox;
+        public ImageView _crimeImage;
+
+        UUID _crimeId;
+        int _position;
+
+        public CrimeListViewHolder(View itemView) {
+            super(itemView);
+
+            _titleTextView = (TextView)
+                    itemView.findViewById(R.id.list_item_crime_title_text_view);
+            _solvedCheckBox = (CheckBox)
+                    itemView.findViewById(R.id.list_item_crime_solved_check_box);
+
+            _dateTextView = (TextView)
+                    itemView.findViewById(R.id.list_item_crime_date_text_view);
+
+            _crimeImage = (ImageView)
+                    itemView.findViewById(R.id.list_item_crime_photo);
+
+            _solvedCheckBox.setOnCheckedChangeListener(this);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Crime crime) {
+            _crimeId = crime.getId();
+
+            _titleTextView.setText(crime.getTitle());
+            _dateTextView.setText(CrimeDateFormat.toFullDate(getActivity(), crime.getCrimeDate()));
+            _solvedCheckBox.setChecked(crime.isSolved());
+
+            File crimeImageFile = CrimeLab.getInstance(getActivity()).getPhotoFile(crime);
+            if(crimeImageFile.exists()) {
+                _crimeImage.setImageBitmap(PictureUtils.getScaledBitmap(crimeImageFile.getPath(), 200, 200));
+                _crimeImage.setVisibility(View.VISIBLE);
+            } else {
+                _crimeImage.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "send position : " + _position);
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), _crimeId);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            CrimeLab crimeLab = CrimeLab.getInstance(getActivity());
+            Crime crime = crimeLab.getCrimeById(_crimeId);
+
+            crime.setSolved(isChecked);
+            crimeLab.updateCrime(crime);
+        }
+    }
+
+    private class CrimeListAdapter
+            extends RecyclerView.Adapter<CrimeListViewHolder> {
+        private static final String TAG = "CrimeListAdapter";
+        private List<Crime> _crimes;
+        private int _viewCreatingCount;
+
+        public CrimeListAdapter(List<Crime> crimes) {
+            _crimes = crimes;
+        }
+
+        protected void setCrimes(List<Crime> crimes) {
+            _crimes = crimes;
+        }
+
+        @Override
+        public CrimeListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            _viewCreatingCount++;
+            Log.d(TAG, "Create view holder for CrimeList: creating view time= " + _viewCreatingCount);
+
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View v = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
+            return new CrimeListViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeListViewHolder holder, int position) {
+            Log.d(TAG, "Bind view holder for CrimeList : position = " + position);
+
+            Crime crime = _crimes.get(position);
+            holder.bind(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return _crimes.size();
+        }
+    }
+
 
 }
